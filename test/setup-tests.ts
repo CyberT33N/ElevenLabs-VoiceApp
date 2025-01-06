1743751e0acc836578157ebdbd06764c14f9bd85
CyberT33N <checkGitHubForEmail@gmail.com>
2025-01-06 21:54:17 +0100

    feat(CCS-001): backup

--- test/setup-tests.ts
+++ test/setup-tests.ts
@@ -13,22 +13,42 @@
 ███████████████████████████████████████████████████████████████████████████████
 */
 
-// ==== BOOTSTRAP ====
+/**
+ * @fileoverview Main test setup configuration
+ * Handles the initialization of the test environment and database cleanup
+ * 
+ * @author t33n Software
+ * @copyright 2025 t33n Software
+ * @license MIT
+ */
+
+// 
 import { bootstrap } from '@/src/bootstrap'
 
-// // ==== UTILS ====
+// 
 // import MongoUtils from '@/utils/db/mongodb/MongoUtils'
 
-// // ==== CLASSES ====
+// 
 // import MongooseUtils from '@/utils/db/mongodb/MongooseUtils'
 
+/**
+ * Main setup function for test environment initialization
+ * Bootstraps the application and handles database cleanup if needed
+ * 
+ * @async
+ * @function setup
+ * @returns {Promise<void>} Resolves when setup is complete
+ * @throws {Error} If bootstrap process fails
+ */
 export async function setup() {
-    console.log('[PRETEST] - INIT..')
+    console.log('[PRETEST] - INIT.. ')
 
+    // 
     await bootstrap()
 
+    // 
     // const mongoUtils = await MongoUtils.getInstance(process.env.MONGODB_ETH_DB_NAME)
     // await mongoUtils.dropDatabase()
 
-    console.log('[PRETEST] - Done!')
+    console.log('[PRETEST] - Done! ')
 }
